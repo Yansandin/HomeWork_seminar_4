46d3b05b7c46269aa3de3199147aa31841e5186d
San <yan_san_din@mail.ru>
2023-01-28 01:10:26 +0300

    first commit

---
+++ 27/Program.cs
@@ -0,0 +1,19 @@
+﻿int Summ(int n)
+{
+
+    int result = 0;
+    while (n >= 1)
+    {
+        result = result + (n%10);
+        n = n / 10;
+    }
+    return result;
+}
+int.TryParse(Console.ReadLine(), out int n);
+if(n<0)
+{
+    Console.WriteLine("Число должно быть положительным");
+    return;
+}
+
+Console.WriteLine(Summ(n));
