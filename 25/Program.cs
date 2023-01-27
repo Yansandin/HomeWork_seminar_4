56fab5b18f24fd40c0881728640bc19349720cb3
San <yan_san_din@mail.ru>
2023-01-28 01:10:17 +0300

    first commit

---
+++ 25/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int Pow(int A,int B)
+{     
+    int n=1;
+    int result = 1;
+    while(n<=B)
+    {
+        result=result*A;
+        n++;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите число которое нужно возвести в степень");
+int.TryParse(Console.ReadLine(), out int A);
+Console.WriteLine("Введите степень в которую нужно возвести число");
+int.TryParse(Console.ReadLine(), out int B);
+
+
+int result= Pow(A,B);
+Console.WriteLine($"Результат возведения числа A в степень B: {result}");
