73239ace7d5269188bc25c48de808c6671d3597e
San <yan_san_din@mail.ru>
2023-01-28 01:10:34 +0300

    first commit

---
+++ 29/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] Array()
+{
+    int[] arr = new int[8];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-100,100);
+        Console.Write($"[{arr[0]},{arr[1]},{arr[2]},{arr[3]},{arr[4]},{arr[5]},{arr[6]},{arr[7]}]");
+    }
+    return arr;
+}
+
+
+Console.Write(Array());
+
+
